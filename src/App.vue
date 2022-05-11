785d8525bfbdc9856ebd2bdc24e443ac6da4e816
Duke Luo <dukeluo@outlook.com>
2022-05-11 22:58:00 +0800

    style: add Noto Serif SC font

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,6 @@ import BaseLayer from './components/BaseLayer.vue'
 
 <style>
 body {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   margin: 0;
 }
 
