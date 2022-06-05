5388eb40e7f5b0c2508a4e399f51627bc188906e
Duke Luo <dukeluo@outlook.com>
2022-06-05 18:02:30 +0800

    chore: update icons

--- manifest.config.ts
+++ manifest.config.ts
@@ -1,7 +1,7 @@
 export default {
   manifest_version: 2,
   name: '歪脖子标签页',
-  description: '一款可以预防颈椎病的新标签页',
+  description: '一款可以预防颈椎病的新标签页。',
   version: process.env.npm_package_version,
   content_security_policy: "script-src 'self'; object-src 'self';",
   chrome_url_overrides: {
@@ -12,6 +12,7 @@ export default {
     default_title: '歪脖子标签页',
   },
   icons: {
+    '16': 'icons/logo16.png',
     '32': 'icons/logo32.png',
     '48': 'icons/logo48.png',
     '64': 'icons/logo64.png',
