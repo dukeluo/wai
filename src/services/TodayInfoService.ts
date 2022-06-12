368cbb58a099531eab824c41d2b7aa19604057d9
Duke Luo <dukeluo@outlook.com>
2022-06-12 22:19:28 +0800

    fix: request today info api with https

--- src/services/TodayInfoService.ts
+++ src/services/TodayInfoService.ts
@@ -2,6 +2,6 @@ import { ApiService } from './ApiService'
 
 export class TodayInfoService extends ApiService {
   constructor(date: string) {
-    super(`http://timor.tech/api/holiday/info/${date}`)
+    super(`https://timor.tech/api/holiday/info/${date}`)
   }
 }
