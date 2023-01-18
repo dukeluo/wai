067d4d5206366e915bd2d3792d75665b2bbd8d0a
Duke Luo <dukeluo@outlook.com>
2023-01-18 21:19:50 +0800

    fix: detect runtime by ext id

--- src/helpers/storage.ts
+++ src/helpers/storage.ts
@@ -2,17 +2,17 @@ const webGetItem = async (key: string): Promise<string | null> => localStorage.g
 
 const webSetItem = (key: string, value: string) => localStorage.setItem(key, value)
 
-const chromeGetItem = async (key: string): Promise<string | null> => {
+const extGetItem = async (key: string): Promise<string | null> => {
   const values = await chrome.storage.sync.get(key)
 
   return values[key]
 }
 
-const chromeSetItem = (key: string, value: string) => chrome.storage.sync.set({ [key]: value })
+const extSetItem = (key: string, value: string) => chrome.storage.sync.set({ [key]: value })
 
-const isChrome = window.chrome !== undefined
+const isWebExt = window?.chrome?.runtime?.id !== undefined
 
 export const storage = {
-  getItem: isChrome ? chromeGetItem : webGetItem,
-  setItem: isChrome ? chromeSetItem : webSetItem,
+  getItem: isWebExt ? extGetItem : webGetItem,
+  setItem: isWebExt ? extSetItem : webSetItem,
 }
