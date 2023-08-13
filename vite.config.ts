eb23eae7504b8c1050d9733e3279a66961d50149
Huan Luo <dukeluo@outlook.com>
2023-08-13 22:28:39 +0800

    chore: remove vite-plugin-css-injected-by-js

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueTypeImports from 'vite-plugin-vue-type-imports'
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 import { viteBuildManifest } from './scripts/build-manifest'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VueTypeImports(), viteBuildManifest(), cssInjectedByJsPlugin()],
+  plugins: [vue(), VueTypeImports(), viteBuildManifest()],
   build: {
     chunkSizeWarningLimit: 4096,
   },
