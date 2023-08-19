42148d3b47a4c0f50841945eb78615ceba90bb04
Huan Luo <dukeluo@outlook.com>
2023-08-19 11:23:45 +0800

    chore: add banner

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import banner from 'vite-plugin-banner'
 import VueTypeImports from 'vite-plugin-vue-type-imports'
 import { viteBuildManifest } from './scripts/build-manifest'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VueTypeImports(), viteBuildManifest()],
+  plugins: [
+    vue(),
+    VueTypeImports(),
+    banner('@author Huan Luo <dukeluo@outlook.com> (https://shaiwang.life)'),
+    viteBuildManifest(),
+  ],
   build: {
     chunkSizeWarningLimit: 4096,
   },
