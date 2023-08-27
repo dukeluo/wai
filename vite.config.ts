cbd7561c70cf7501eabe8d80a07f250336aa7ded
Huan Luo <dukeluo@outlook.com>
2023-08-27 13:53:56 +0800

    chore: upgrade vue and vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import banner from 'vite-plugin-banner'
-import VueTypeImports from 'vite-plugin-vue-type-imports'
 import { viteBuildManifest } from './scripts/build-manifest'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueTypeImports(),
-    banner('@author Huan Luo <dukeluo@outlook.com> (https://shaiwang.life)'),
-    viteBuildManifest(),
-  ],
+  plugins: [vue(), banner('@author Huan Luo <dukeluo@outlook.com> (https://shaiwang.life)'), viteBuildManifest()],
   build: {
     chunkSizeWarningLimit: 4096,
   },
